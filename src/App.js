c02bc2b33d8e54143c1fd7767f3f11f3ba4e51a2
hongboy1 <hongboy15@gmail.com>
2023-01-09 16:04:22 +0900

    리액트_계산기(2023-01-09) 버튼 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <header>
+        <h1> react_calculator </h1>
+    </header>
+}
+
+function Calculator(){
+  return <div>
+    <input/>
+    <div>
+      <button>C</button>
+    </div>
+    <div>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>/</button>
+    </div>
+    <div>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>X</button>
+    </div>
+    <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>-</button>
+    </div>
+    <div>
+      <button>0</button>
+      <button>.</button>
+      <button>=</button>
+      <button>+</button>
+    </div>
+  </div>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Calculator></Calculator>
     </div>
   );
 }
