7ee7897f379fa2ccd4481cd98f2a2ea3bb08607a
hongboy1 <hongboy15@gmail.com>
2023-01-11 16:42:37 +0900

    리액트_계산기(2023-01-11) 기능 추가 및 디자인 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import { BsBackspace  } from "react-icons/bs";
+import * as events from "events";
 
 function Header(){
   return <header>
@@ -8,45 +10,102 @@ function Header(){
     </header>
 }
 
+let chk = false;
 function Calculator(){
+  /**첫 번쨰 숫자*/
+  const [num,setNum] = useState("");
+  /**두 번쨰 숫자*/
+  const [scnum, SetScnum]= useState("");
+  /**답 번쨰 숫자*/
+  const [dab, setDab] = useState("");
+  /**첫 번쨰 여부 체크*/
+  const [operchk, setOperchk] = useState(false);
+  /**실행 횟수*/
+  const [cnt, setCnt] = useState(0);
 
-  const [num,cnt] = useState("");
-  const cntUp = (Up) => {
-    if(Up=="C"){
-      cnt((num - num)+" ");
+
+  /**숫자 가져오기*/
+  const getNum = (e) => {
+    if(operchk === true){
+      SetScnum((prev) => prev + e.target.value);
     }
-    else cnt(num + Up);
+    else {
+      setNum((prev) => prev + e.target.value);
+    }
+
   };
+  /**기호 가져오기*/
+  const getOper = (e) => {
+    if (num.length === 0) {
+      return;
+    }
+    setOperchk(true);
+    if(e.target.value == "=" && cnt!=0) setDab((prev) =>  dab + " "+ scnum +" "+ e.target.value)
+    else {
+      setCnt(cnt+1);
+      setDab((prev) => num + " " + e.target.value);
+    }
+  };
+
+  /**다 지우기*/
+  const allClear = () => {
+    setOperchk(false);
+    setNum((prev) => "");
+    setDab((prev) => "");
+  };
+
+  /**한개씩 지우기*/
+  const delCalc = () => { 
+    setOperchk(false);
+    let str = String(num).slice(0, -1);
+    setNum((prev) => str);
+  };
+
+  /**input 출력 창*/
+  let content = null;
+  if(operchk === true){
+    content = <input className="text" type={"text"} value={scnum}/>
+  }else{
+    content = <input className="text" type={"text"} value={num}/>
+  }
+
   return <div className="Button">
-    <input className="text" type={"text"} value={num}/>
     <div>
-      <button className="teg" onClick={(e)=>{cntUp("C")}}>C</button>
-      <button className="teg">+/-</button>
-      <button className="teg">%</button>
-      <button className="teg">&divide;</button>
+      <input className="text2" type={"text"} value={dab}/>
+    </div>
+    <div>{content}</div>
+    <div>
+
+      <button className="teg" value={"%"} onClick={getOper}>%</button>
+      <button className="teg" onClick={allClear}>C</button>
+      <button className="teg" onClick={delCalc} style={{fontSize : "20px"}}>
+        <BsBackspace style={{marginTop : "5px"}}></BsBackspace>
+      </button>
+      <button className="teg" value={"&divide;"} onClick={getOper}>&divide;</button>
     </div>
     <div>
-      <button onClick={(e)=>{cntUp(7)}}>7</button>
-      <button onClick={(e)=>{cntUp(8)}}>8</button>
-      <button onClick={(e)=>{cntUp(9)}}>9</button>
-      <button className="teg">X</button>
+      <button value={7} onClick={getNum}>7</button>
+      <button value={8} onClick={getNum}>8</button>
+      <button value={9} onClick={getNum}>9</button>
+      <button value={"x"} onClick={getOper} className="teg" >X</button>
     </div>
     <div>
-      <button onClick={(e)=>{cntUp(4)}}>4</button>
-      <button onClick={(e)=>{cntUp(5)}}>5</button>
-      <button onClick={(e)=>{cntUp(6)}}>6</button>
-      <button className="teg">-</button>
+      <button value={4} onClick={getNum}>4</button>
+      <button value={5} onClick={getNum}>5</button>
+      <button value={6} onClick={getNum}>6</button>
+      <button value={"-"} onClick={getOper} className="teg">-</button>
     </div>
     <div>
-      <button onClick={(e)=>{cntUp(1)}}>1</button>
-      <button onClick={(e)=>{cntUp(2)}}>2</button>
-      <button onClick={(e)=>{cntUp(3)}}>3</button>
-      <button className="teg">+</button>
+      <button value={1} onClick={getNum}>1</button>
+      <button value={2} onClick={getNum}>2</button>
+      <button value={3} onClick={getNum}>3</button>
+      <button value={"+"} onClick={getOper} className="teg">+</button>
     </div>
     <div>
-      <button style={{width : "165px"}} onClick={(e)=>{cntUp(0)}}>0</button>
-      <button className="teg">.</button>
-      <button style={{background : "#1d6978", color:"white"}}>=</button>
+      <button value={"+/-"} onClick={getOper}>+/-</button>
+      <button value={0} onClick={getNum}>0</button>
+      <button value={"."} onClick={getOper} className="teg">.</button>
+      <button value={"="} onClick={getOper} style={{background : "#1d6978", color:"white"}}>=</button>
     </div>
   </div>
 }
