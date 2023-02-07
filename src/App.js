2bbde2c33ee77c86eb5c17d4035c0feb7225f230
hongboy1 <hongboy15@gmail.com>
2023-02-07 16:07:11 +0900

    리액트_계산기(2023-02-07) 버그 수정 (미완)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, {useRef, useState} from 'react';
 import { BsBackspace  } from "react-icons/bs";
 import * as events from "events";
 
@@ -12,82 +12,106 @@ function Header(){
 
 let chk = false;
 function Calculator(){
-  /**첫 번쨰 숫자*/
-  const [num,setNum] = useState("");
-  /**두 번쨰 숫자*/
-  const [scnum, SetScnum]= useState("");
-  /**답 번쨰 숫자*/
-  const [dab, setDab] = useState("");
-  /**첫 번쨰 여부 체크*/
-  const [operchk, setOperchk] = useState(false);
-  /**실행 횟수*/
-  const [cnt, setCnt] = useState(0);
-
-
-  /**숫자 가져오기*/
+  /*
+  /!**숫자 가져오기*!/
   const getNum = (e) => {
-    if(operchk === true){
-      SetScnum((prev) => prev + e.target.value);
-    }
-    else {
-      setNum((prev) => prev + e.target.value);
-    }
-
+    setNum(num + e.target.value);
+    setResult(result + e.target.value);
   };
-  /**기호 가져오기*/
+  /!**기호 가져오기*!/
   const getOper = (e) => {
-    if (num.length === 0) {
-      return;
+    if(num.length===0){
+      setNum("0" + e.target.value);
     }
-    setOperchk(true);
-    if(e.target.value == "=" && cnt!=0) setDab((prev) =>  dab + " "+ scnum +" "+ e.target.value)
     else {
-      setCnt(cnt+1);
-      setDab((prev) => num + " " + e.target.value);
+
     }
   };
 
-  /**다 지우기*/
-  const allClear = () => {
-    setOperchk(false);
-    setNum((prev) => "");
-    setDab((prev) => "");
-  };
-
-  /**한개씩 지우기*/
-  const delCalc = () => { 
-    setOperchk(false);
-    let str = String(num).slice(0, -1);
-    setNum((prev) => str);
-  };
-
-  /**input 출력 창*/
-  let content = null;
-  if(operchk === true){
-    content = <input className="text" type={"text"} value={scnum}/>
-  }else{
-    content = <input className="text" type={"text"} value={num}/>
+  const getNum = (e) => {
+    let value = e.target.value;
+    setInput(input + value);
+    setShow(show + value);
+    setResult(result+value);
   }
+  const getOper = (e) => {
+    setInput((input) => input + e.target.value);
+    setShow(show + e.target.value);
+      setResult("");
+  };
+  const calculate = (e) => {
+    let value = e.target.value;
+    setShow(show + value);
+    setResult(eval(input));
+  }
+
+  const clear = () => {
+    setInput('');
+    setShow('');
+    setResult("");
+  }
+
+
+
+*/
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [oper,setOper] = useState('');
+  const [inputNum, setInputNum] = useState(0);
+
+
+  const [show, setShow] = useState('');
+  const [chk, setChk] = useState(false);
+  const [result, setResult] = useState("");
+
+  const getNum = (e) => {
+    if(inputNum===0){
+      setInput1((prev) =>prev + e.target.value);
+    }
+    else{
+      setInput2((prev) =>prev + e.target.value);
+      setInputNum(2);
+    }
+  }
+  const getOper = (e) => {
+    if(input1.length === 0) return;
+    setOper(e.target.value);
+    setInputNum(1);
+  };
+  const calculate = ( ) => {
+    setResult(eval(moon))
+  }
+
+  const clear = () => {
+    setInput1('');
+    setInput2('');
+    setResult('');
+    setOper('');
+
+  }
+
+  const moon = input1 + oper + input2;
 
   return <div className="Button">
     <div>
-      <input className="text2" type={"text"} value={dab}/>
+      <input className="text2" type={"text"} value={moon}/>
     </div>
-    <div>{content}</div>
     <div>
-
+      <input className="text" type={"text"} value={result}/>
+    </div>
+  <div>
       <button className="teg" value={"%"} onClick={getOper}>%</button>
-      <button className="teg" onClick={allClear}>C</button>
-      <button className="teg" onClick={delCalc} style={{fontSize : "20px"}}>
+      <button className="teg" onClick={clear}>C</button>
+      <button className="teg" onClick={getNum} style={{fontSize : "20px"}}>
         <BsBackspace style={{marginTop : "5px"}}></BsBackspace>
       </button>
-      <button className="teg" value={"&divide;"} onClick={getOper}>&divide;</button>
+      <button className="teg" value={"/"} onClick={getOper}>&divide;</button>
     </div>
     <div>
       <button value={7} onClick={getNum}>7</button>
       <button value={8} onClick={getNum}>8</button>
       <button value={9} onClick={getNum}>9</button>
-      <button value={"x"} onClick={getOper} className="teg" >X</button>
+      <button value={"*"} onClick={getOper} className="teg" >X</button>
     </div>
     <div>
       <button value={4} onClick={getNum}>4</button>
@@ -105,7 +129,7 @@ function Calculator(){
       <button value={"+/-"} onClick={getOper}>+/-</button>
       <button value={0} onClick={getNum}>0</button>
       <button value={"."} onClick={getOper} className="teg">.</button>
-      <button value={"="} onClick={getOper} style={{background : "#1d6978", color:"white"}}>=</button>
+      <button value={"="} onClick={calculate} style={{background : "#1d6978", color:"white"}}>=</button>
     </div>
   </div>
 }
